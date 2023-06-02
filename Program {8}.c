53a56f04b4e92f6ffb8ff236ba2ecdd2d109c081
umarmaul <umarmaulana31@gmail.com>
2023-06-02 23:09:02 +0700

    kelar

--- Program {8}.c
+++ Program {8}.c
@@ -24,8 +24,8 @@ Node *createNode(const char *name, int day, int month, int year) {
     return newNode;
 }
 
-void enqueue(Queue *queue, const char *task, int day, int month, int year) {
-    Node *newNode = createNode(task, day, month, year);
+void enqueue(Queue *queue, const char *name, int day, int month, int year) {
+    Node *newNode = createNode(name, day, month, year);
 
     if (queue->front == NULL) {
         queue->front = queue->rear = newNode;
@@ -46,12 +46,12 @@ void enqueue(Queue *queue, const char *task, int day, int month, int year) {
         }
     }
 
-    printf("Task added to the queue.\n");
+    printf("\nPesanan baru telah masuk ke daftar antrian.\n");
 }
 
 void dequeue(Queue *queue) {
     if (queue->front == NULL) {
-        printf("The queue is empty.\n");
+        printf("Antrian kosong.\n");
         return;
     }
 
@@ -63,12 +63,12 @@ void dequeue(Queue *queue) {
     }
 
     free(temp);
-    printf("Task removed from the queue.\n");
+    printf("\nPesanan telah dihapus dari antrian.\n\n");
 }
 
 void displayQueue(Queue *queue) {
     if (queue->front == NULL) {
-        printf("The queue is empty.\n");
+        printf("Pesanan kosong.\n");
         return;
     }
 
@@ -92,22 +92,22 @@ void freeQueue(Queue *queue) {
     queue->rear = NULL;
 }
 
-void editTask(Node *head, int taskIndex) {
+void editName(Node *head, int taskIndex) {
     Node *temp = head;
     int index = 1;
 
     while (temp != NULL) {
         if (index == taskIndex) {
-            printf("Enter new name: ");
+            printf("Masukkan nama baru: ");
             scanf(" %[^\n]s", temp->name);
-            printf("Task edited.\n");
+            printf("\nNama baru telah tersimpan.\n");
             return;
         }
         temp = temp->next;
         index++;
     }
 
-    printf("Invalid name index.\n");
+    printf("Permintaan tidak valid.\n");
 }
 
 void editDate(Node *head, int taskIndex) {
@@ -116,9 +116,9 @@ void editDate(Node *head, int taskIndex) {
 
     while (temp != NULL) {
         if (index == taskIndex) {
-            printf("Enter new due date (DD MM YYYY): ");
+            printf("Masukkan tanggal baru (DD MM YYYY): ");
             scanf("%d %d %d", &temp->day, &temp->month, &temp->year);
-            printf("Date edited.\n");
+            printf("\nTanggal telah diubah.\n");
         return;
         
     }
@@ -126,7 +126,7 @@ void editDate(Node *head, int taskIndex) {
     index++;
 	}	
 
-	printf("Invalid name index.\n");
+	printf("Permintaan tidak valid.\n");
 }
 
 void freeList(Node *head) {
@@ -138,7 +138,7 @@ void freeList(Node *head) {
 	}
 }
 
-void cetakTanggal(int day, int month, int year, int kalender[]){
+void cetakTanggal(int day, int month, int year){
 	char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}; // Array untuk nama-nama bulan
     int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // Array untuk jumlah tanggal dalam setiap bulan
 	int i, j, firstDayOfMonth;
@@ -173,11 +173,7 @@ void cetakTanggal(int day, int month, int year, int kalender[]){
         }
     
 	// Mencetak tanggal
-    if (kalender[i-1] == 1) {
-        printf(" %2d*", i);
-        } else {
-            printf(" %2d ", i);
-            }
+    printf(" %2d ", i);
     }
     
 	printf("\n");
@@ -193,25 +189,29 @@ int main() {
 	int taskIndex;
 
     int i;
-    int kalender[31] = {0}; // Array untuk menyimpan penyewa pada tiap tanggal
-     
-     for(i = 0; i < 50; i++){
+    for(i = 0; i < 50; i++){
         	printf("=");
 		}
-        printf("\n\tProgram Pre-Order Kue Ulang Tahun\n");
-        printf("\n\tKelompok 8:\n");
-        printf("\tDaniel Valerian\t- 2106728295 \n\tElvina Sunardi\t- 2106706413 \n\tUmar Maulana\t- 2106631034\n");
-        for(i = 0; i < 50; i++){
-        	printf("=");
-		}
+    printf("\n\tProgram Pre-Order Kue Ulang Tahun\n");
+    printf("\n\tKelompok 8:\n");
+    printf("\tDaniel Valerian\t- 2106728295 \n\tElvina Sunardi\t- 2106706413 \n\tUmar Maulana\t- 2106631034\n");
+    for(i = 0; i < 50; i++){
+        printf("=");
+	}
     printf("\n\n");
 
     system("pause");
     system("cls");
     
 do {
-    printf("\nMenu:\n");
-    printf("1. Masukkan pesanan\n");
+     for(i = 0; i < 20; i++){
+        	printf("=");
+		}
+    printf("\n\tMenu\n");
+     for(i = 0; i < 20; i++){
+        	printf("=");
+		}
+    printf("\n1. Masukkan pesanan\n");
     printf("2. Pesanan selesai\n");
     printf("3. Edit nama pemesan\n");
     printf("4. Edit tanggal pengambilan\n");
@@ -221,37 +221,65 @@ do {
     scanf("%d", &choice);
 
     switch (choice) {
-        case 1:
+        case 1: //Memasukkan pesanan baru
+            system("cls");
             printf("Masukkan nama: ");
             scanf(" %[^\n]s", name);
             printf("Masukkan tanggal pengambilan (DD MM YYYY): ");
             scanf("%d %d %d", &day, &month, &year);
+            cetakTanggal(day, month, year);
             enqueue(&queue, name, day, month, year);
+            system("pause");
+            system("cls");
             break;
-        case 2:
+        case 2: //Menghapus antrian
+            system("cls");
+            printf("Antrian sebelumnya:\n");
+            displayQueue(&queue);
             dequeue(&queue);
+            printf("Antrian sekarang:\n");
+            displayQueue(&queue);
+            system("pause");
+            system("cls");
             break;
-        case 3:
+        case 3: //Edit nama pemesam
+            system("cls");
             displayQueue(&queue);
             printf("Pesanan ke berapa yang ingin diedit: ");
             scanf("%d", &taskIndex);
-            editTask(queue.front, taskIndex);
+            editName(queue.front, taskIndex);
+            system("pause");
+            system("cls");
             break;
-        case 4:
+        case 4: //Edit tanggal pengambilan
+            system("cls");
             displayQueue(&queue);
-            printf("Enter name index to edit the due date: ");
+            printf("Pesanan ke berapa yang ingin diedit: ");
             scanf("%d", &taskIndex);
             editDate(queue.front, taskIndex);
+            system("pause");
+            system("cls");
             break;
-            case 5:
+        case 5: //Menampilkan pesanan
+            system("cls");
+            cetakTanggal(day, month, year);
+            printf("\n");
             displayQueue(&queue);
+            system("pause");
+            system("cls");
             break;
-        case 6:
+        case 6: //Keluar
+            system("cls");
             freeQueue(&queue);
             printf("Exiting program.\n");
+            system("pause");
+            system("cls");
             break;
         default:
+            system("cls");
             printf("Invalid choice. Please try again.\n");
+            system("pause");
+            system("cls");
             break;
         }
     } while (choice != 6);
