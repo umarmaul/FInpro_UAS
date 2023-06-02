fc54481603aaed0e9d615838cfbe34afa9ce4880
umarmaul <umarmaulana31@gmail.com>
2023-06-02 16:14:16 +0700

    opening

--- Program {8}.c
+++ Program {8}.c
@@ -2,7 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+void printOpening{
+     int i;
+     
+     for(i = 0; i < 45; i++){
+        	printf("=");
+		}
+        printf("\n\tAplikasi Penjualan Mobil Bekas\n");
+        printf("\n\tKelompok 8:\n");
+        printf("\tDaniel Valerian\t- 2106728295 \n\tElvina Sunardi\t- 2106706413 \n\tUmar Maulana\t- 2106631034\n");
+        for(i = 0; i < 45; i++){
+        	printf("=");
+		}
+    printf("\n\n");
+    system("pause");
+    system("cls");
+}
 
+int main(){
+    int i;
+    
     return 0;
 }
